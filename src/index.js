1cf972e659f6c1a241a9db112cbd559adcc15d46
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-10-24 19:28:58 +0100

    Mount App on the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+import React from 'react';
+import { render } from 'react-dom';
+
+// App component
+import App from './components/App';
+
+// Styles
+import './styles/styles.css';
+import '~/bootstrap/dist/css/bootstrap.css';
+
+render(<App />, document.querySelector('#app'));
