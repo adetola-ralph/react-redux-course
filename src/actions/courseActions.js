be862ec6b8e358d8056797589441778c7a0415c5
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-10-26 07:51:51 +0100

    Add action and reducer to save and update course

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,4 @@
-import { LOAD_COURSES_SUCCESS } from './actionTypes';
+import { LOAD_COURSES_SUCCESS, UPDATE_COURSE_SUCCESS, CREATE_COURSE_SUCCESS } from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
 export const loadCoursesSuccess = courses => ({
@@ -6,8 +6,28 @@ export const loadCoursesSuccess = courses => ({
   courses,
 });
 
+export const updateCourseSuccess = course => ({
+  type: UPDATE_COURSE_SUCCESS,
+  course,
+});
+
+export const createCourseSuccess = course => ({
+  type: CREATE_COURSE_SUCCESS,
+  course,
+});
+
 export const loadCourses = () => dispatch => courseApi.getAllCourses().then((courses) => {
   dispatch(loadCoursesSuccess(courses));
 }).catch((error) => {
   throw (error);
+});
+
+export const saveCourse = course => dispatch => courseApi.saveCourse(course).then((savedCourse) => {
+  if (course.id) {
+    dispatch(updateCourseSuccess(savedCourse));
+  } else {
+    dispatch(createCourseSuccess(savedCourse));
+  }
+}).catch((error) => {
+  throw error;
 });
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,10 +1,20 @@
-import { LOAD_COURSES_SUCCESS } from '../actions/actionTypes';
+import { LOAD_COURSES_SUCCESS, CREATE_COURSE_SUCCESS, UPDATE_COURSE_SUCCESS } from '../actions/actionTypes';
 
 const courseReducer = (state = [], action) => {
   const { type } = action;
   switch (type) {
     case LOAD_COURSES_SUCCESS:
       return action.courses;
+    case CREATE_COURSE_SUCCESS:
+      return [...state, { ...action.course }];
+    case UPDATE_COURSE_SUCCESS:
+      return state.map((course) => {
+        if (course.id === action.course.id) {
+          return { ...action.course };
+        }
+
+        return course;
+      });
     default:
       return state;
   }
