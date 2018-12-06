3b4cf484ba575fbe757c9ecfd737b879a2c6b709
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-12-06 21:34:10 +0100

    Tests for Author and Course Form

---
+++ src/components/author/__tests__/AuthorForm.spec.js
@@ -0,0 +1,21 @@
+import { shallow, mount } from 'enzyme';
+
+import AuthorForm from '../AuthorForm';
+
+const onSave = jest.fn();
+const onChange = jest.fn();
+
+const props = {
+  author: {},
+  onSave,
+  onChange,
+};
+
+describe('Course Form component', () => {
+  test('test events from the form', () => {
+    const wrapper = shallow(<AuthorForm {...props} />);
+
+    wrapper.find('form').simulate('submit');
+    expect(onSave).toHaveBeenCalled();
+  });
+});
---
+++ src/components/course/__tests__/CourseForm.spec.js
@@ -0,0 +1,22 @@
+import { shallow, mount } from 'enzyme';
+
+import CourseForm from '../CourseForm';
+
+const onSave = jest.fn();
+const onChange = jest.fn();
+
+const props = {
+  course: {},
+  allAuthors: [],
+  onSave,
+  onChange,
+};
+
+describe('Course Form component', () => {
+  test('test events from the form', () => {
+    const wrapper = shallow(<CourseForm {...props} />);
+
+    wrapper.find('form').simulate('submit');
+    expect(onSave).toHaveBeenCalled();
+  });
+});
